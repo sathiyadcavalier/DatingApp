f541a4485c640c28d259a1f20b0e535f1c83200f
SSR <SSR@gmail.com>
2024-01-29 00:15:41 +0530

    End of Section 5

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
